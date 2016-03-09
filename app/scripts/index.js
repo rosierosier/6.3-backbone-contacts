f3e71007ac92d91faa7877dd636f5c76ba273733
Gabriel Pages <gbpages@yahoo.com>
2016-03-09 15:54:44 -0500

    submit button

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -9,3 +9,14 @@ var models = require('./models/contacts');
 var contacts = new models.ContactCollection();
 var contactView = new Contact({collection: contacts, el: $('#contacts')[0]});
 console.log(contactView);
+
+
+$('#submit-btn').on ('click', function(){
+  var name = $('#first').val() + $('#last').val();
+  var email = $('#email').val();
+  var number = $('#number').val();
+  var twitter = $('#twitter').val();
+  var linkedin = $('#linkedin').val();
+  var contactInfo = {'name': name, 'email': email, 'number': number, 'twitter': twitter, 'linkedin': linkedin};
+  console.log(contactInfo);
+});
